172a51fa4c509719464bca0863b679420aa9fb33
s187963 <93327161+s187963@users.noreply.github.com>
2021-10-28 14:02:46 +0100

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -19,7 +19,7 @@ public class Addition {
       System.out.printf("Sum is %d%n", sum); // display sum
    } // end method main
 } // end class Addition
-//Hello my name is Jakub Capture
+//Hello my name is Jakub 
 
 /**************************************************************************
  * (C) Copyright 1992-2018 by Deitel & Associates, Inc. and               *
